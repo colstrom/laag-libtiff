c8e3662d5ad83ad6b614e37db3eae5bea2ac53f2
Chris Olstrom <chris@olstrom.com>
2018-06-01 13:52:09 -0700

    Build with laag-xz

--- ext/laag/libtiff/extconf.rb
+++ ext/laag/libtiff/extconf.rb
@@ -4,10 +4,19 @@
 require 'mkmf' # Ruby Standard Library
 
 require 'laag' # MIT License
+require 'laag/xz' # Public Domain
+
 require_relative '../../../lib/laag/libtiff'
 
-LAAG::BuildEnvironment
-  .new(LAAG.libtiff)
-  .script { default! }
+LAAG::BuildEnvironment.new(LAAG.libtiff).script do
+  xz_include = File.join(LAAG.xz.install_path, "include")
+  xz_lib     = File.join(LAAG.xz.install_path, "lib")
+
+  default!(
+    configure: [
+      "--with-lzma-include-dir=#{xz_include}",
+      "--with-lzma-lib-dir=#{xz_lib}",
+    ])
+end
 
 create_makefile 'laag/libtiff'
